a7dc673aaa952bc5b2da667bfd826625fbd1e487
suyeon419 <lsyeon030419@kumoh.ac.kr>
2025-10-22 16:22:12 +0900

    Footer 틀!
    확정 아님

--- iLog/src/components/Footer.jsx
+++ iLog/src/components/Footer.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div style={{ backgroundColor: '#F5F1EC' }}>
+            <div style={{ marginBottom: '6px', fontWeight: '600', color: '#b66e03' }}>
+                iLog © {new Date().getFullYear()}
+            </div>
+            <div style={{ color: '#7a7a7a' }}>
+                문의:{' '}
+                <a href="mailto:ilog.help@gmail.com" style={{ color: '#b66e03', textDecoration: 'none' }}>
+                    ilog.help@gmail.com
+                </a>
+            </div>
+            <div style={{ marginTop: '5px' }}>
+                <a href="/privacy" style={{ color: '#7a7a7a', margin: '0 10px', textDecoration: 'none' }}>
+                    개인정보 처리방침
+                </a>{' '}
+                |
+                <a href="/terms" style={{ color: '#7a7a7a', margin: '0 10px', textDecoration: 'none' }}>
+                    이용약관
+                </a>
+            </div>
+        </div>
+    );
 }
